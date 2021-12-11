91dcb6549a04a85c7bb3d39a7cd541b0e37dedb3
saimeerasingh <singhsaimeera@gmail.com>
2021-12-11 23:57:56 +0000

    blueprint method added to destinations

--- controllers/destinations_controllers.py
+++ controllers/destinations_controllers.py
@@ -1,3 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.destination import Destination
+
+import repositories.destination_repository as destination_repository
+
+destinations_blueprint = Blueprint("destinations",__name__)
+
+@destinations_blueprint.route('/destinations')
+def destinations():
+    destinations = destination_repository.select_all()
+    return render_template('destinations/index.html', destinations = destinations)
+    
