9d6f5f05227e8588bfb92a54055064f0b233f38e
saimeerasingh <singhsaimeera@gmail.com>
2021-12-11 22:13:07 +0000

    CRUD methods added for countries file

--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -20,4 +20,22 @@ def select_all():
     return countries
 
 def select(id):
+    sql = "SELECT * FROM countries WHERE id = %s"
+    values = [id]
+    result = run_sql(sql,values)[0]
+    country = Country(result["name"],result["continent"],result["id"])
+    return country
+
+def delete_all():
+    sql = "DELETE FROM countries"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM countries WHERE id =%s"
+    values = [id]
+    run_sql(sql,values)
     
+def update(country):
+    sql = "UPDATE coutries SET(name,continent,visited) VALUES (%s ,%s ,%s) WHERE id =%s "
+    values = [country.name,country.continent,country.visited,country.id]
+    run_sql(sql,values)
