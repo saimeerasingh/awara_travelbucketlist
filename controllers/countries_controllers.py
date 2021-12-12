b3a25d82ed9fcc494d3a3175a73b217340c75437
saimeerasingh <singhsaimeera@gmail.com>
2021-12-12 17:26:14 +0000

    html  updated

--- controllers/countries_controllers.py
+++ controllers/countries_controllers.py
@@ -11,4 +11,24 @@ def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", all_countries = countries)
 
+# NEW
+# GET '/countries/new'
+# @countries_blueprint.route('/countries/new', methods =['GET'])
+# def new_country():
+#     countries = country_repository.select_all
+    
 
+# CREATE
+# POST '/countries
+
+# SHOW
+# GET '/countries/<id>'
+
+# EDIT
+# GET '/countries/<id>/edit'
+
+# UPDATE
+# PUT '/countries/<id>'
+
+# DELETE
+# DELETE '/countries/<id>'
--- controllers/destinations_controllers.py
+++ controllers/destinations_controllers.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from controllers.cities_controllers import cities
+from controllers.countries_controllers import countries
+from models.city import City
 from models.destination import Destination
+import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
+import sys
+from models.country import Country
 
 import repositories.destination_repository as destination_repository
 
@@ -10,4 +17,32 @@ destinations_blueprint = Blueprint("destinations",__name__)
 def destinations():
     destinations = destination_repository.select_all()
     return render_template('destinations/index.html', all_destinations = destinations)
+
+# NEW
+# GET '/destinations/new'
+@destinations_blueprint.route('/destinations/new', methods = ['POST'])
+def new_destinations():
+    country =  Country(request.form['country'],request.form['continent'],False)
+    country_id = country_repository.save(country)
+    print(countries,file=sys.stderr)
+
+    city = City(request.form['city'],False,country_id=country_id)
+    city_repository.save(city)
+    return redirect('/')
+    
+
+# CREATE
+# POST '/countries
+
+# SHOW
+# GET '/countries/<id>'
+
+# EDIT
+# GET '/countries/<id>/edit'
+
+# UPDATE
+# PUT '/countries/<id>'
+
+# DELETE
+# DELETE '/countries/<id>'
 
